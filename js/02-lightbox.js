8fe42b267453f9944fa4336a79bcb6a49e99063a
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-03-24 12:10:43 +0200

    done 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,43 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const ulRef = document.querySelector('.gallery')
+const galleryCardsSet = createGallery(galleryItems);
+
+
+function createGallery(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => {
+            return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </div>`;
+        })
+        .join('');
+}
+
+ulRef.insertAdjacentHTML('beforeend', galleryCardsSet)
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+const onKeydownEsc = event => {
+    console.log(event.code);
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+};
+
+instance.show();
